dfe61f49d5d98260cce39a1d2b5e46dd4aead22b
ryanxsangha <118231236+ryanxsangha@users.noreply.github.com>
2024-08-21 22:08:32 -0700

    Update validPassword.py

--- validPassword/validPassword.py
+++ validPassword/validPassword.py
@@ -1,3 +1,25 @@
 import random
 
-c, l, s, n = 0, 0, 0, 0
+c, l, s, n = 0, 0, 0, 0 #initialize count for uppercase, lowercase, special chars, nums
+
+newpassword = input('Enter password: ')
+capletters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowerletters = 'abcdefghijklmnopqrstuvwxyz'
+specialchar = '!@#$%^&*()?'
+nums = '1234567890'
+
+if(len(newpassword) >= 10):
+    for i in newpassword:
+        if(i in capletters):
+            c+=1
+        if(i in lowerletters):
+            l+=1
+        if(i in specialchar):
+            s+=1
+        if(i in nums):
+            n+=1
+
+if(c>0 and l>0 and s>0 and n>0 and c+l+s+n == len(newpassword)):
+    print('Valid password')
+else:
+    print('Password must contain a capital and lowercase letter, digit, a special character, and be at least 10 characters. Try again')
